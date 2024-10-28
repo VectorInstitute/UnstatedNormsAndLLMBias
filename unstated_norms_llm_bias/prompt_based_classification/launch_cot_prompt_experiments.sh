7f1407300870f65207a404a6b36d8054dfd3c47d
David Emerson <david.emerson@vectorinstitute.ai>
2024-10-28 10:17:42 -0400

    Adding in scripts and code for prompting experiments with llama3 and mistral

--- unstated_norms_llm_bias/prompt_based_classification/launch_cot_prompt_experiments.sh
+++ unstated_norms_llm_bias/prompt_based_classification/launch_cot_prompt_experiments.sh
@@ -17,15 +17,29 @@ DATASET=$2
 
 # Run the ID and DATASET for each of the llama, and llama2 models
 
-# LLaMA
-SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment_cot.slrm \
+# # LLaMA
+# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment_cot.slrm \
+#     ${RUN_ID} \
+#     ${DATASET}"
+# echo "Running sbatch command ${SBATCH_COMMAND}"
+# sbatch ${SBATCH_COMMAND}
+
+# # Llama 2
+# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama2_experiment_cot.slrm \
+#     ${RUN_ID} \
+#     ${DATASET}"
+# echo "Running sbatch command ${SBATCH_COMMAND}"
+# sbatch ${SBATCH_COMMAND}
+
+# Llama 3
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama3_experiment_cot.slrm \
     ${RUN_ID} \
     ${DATASET}"
 echo "Running sbatch command ${SBATCH_COMMAND}"
 sbatch ${SBATCH_COMMAND}
 
-# Llama 2
-SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama2_experiment_cot.slrm \
+# Mistral
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_mistral_experiment_cot.slrm \
     ${RUN_ID} \
     ${DATASET}"
 echo "Running sbatch command ${SBATCH_COMMAND}"
--- unstated_norms_llm_bias/prompt_based_classification/launch_prompt_experiments.sh
+++ unstated_norms_llm_bias/prompt_based_classification/launch_prompt_experiments.sh
@@ -17,22 +17,36 @@ DATASET=$2
 
 # Run the ID and DATASET for each of the opt, llama, and llama2 models
 
-# OPT
-SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_opt_experiment.slrm \
+# # OPT
+# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_opt_experiment.slrm \
+#     ${RUN_ID} \
+#     ${DATASET}"
+# echo "Running sbatch command ${SBATCH_COMMAND}"
+# sbatch ${SBATCH_COMMAND}
+
+# # LLaMA
+# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment.slrm \
+#     ${RUN_ID} \
+#     ${DATASET}"
+# echo "Running sbatch command ${SBATCH_COMMAND}"
+# sbatch ${SBATCH_COMMAND}
+
+# # Llama 2
+# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama2_experiment.slrm \
+#     ${RUN_ID} \
+#     ${DATASET}"
+# echo "Running sbatch command ${SBATCH_COMMAND}"
+# sbatch ${SBATCH_COMMAND}
+
+# Llama 3
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama3_experiment.slrm \
     ${RUN_ID} \
     ${DATASET}"
 echo "Running sbatch command ${SBATCH_COMMAND}"
 sbatch ${SBATCH_COMMAND}
 
-# LLaMA
-SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment.slrm \
-    ${RUN_ID} \
-    ${DATASET}"
-echo "Running sbatch command ${SBATCH_COMMAND}"
-sbatch ${SBATCH_COMMAND}
-
-# Llama 2
-SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama2_experiment.slrm \
+# Mistral
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_mistral_experiment.slrm \
     ${RUN_ID} \
     ${DATASET}"
 echo "Running sbatch command ${SBATCH_COMMAND}"
