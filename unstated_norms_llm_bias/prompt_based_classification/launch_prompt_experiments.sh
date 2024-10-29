86d7cf3bfe247a39c8e49731fa27e861c98c940d
David Emerson <david.emerson@vectorinstitute.ai>
2024-10-29 12:45:20 -0400

    Fixing a few more predictions files. Modifying the visualization notebook and launch scripts

--- unstated_norms_llm_bias/prompt_based_classification/launch_prompt_experiments.sh
+++ unstated_norms_llm_bias/prompt_based_classification/launch_prompt_experiments.sh
@@ -15,28 +15,28 @@
 RUN_ID=$1
 DATASET=$2
 
-# Run the ID and DATASET for each of the opt, llama, and llama2 models
+# Run the ID and DATASET for each of the OPT, llama, llama2, llama3, and Mistral models
 
-# # OPT
-# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_opt_experiment.slrm \
-#     ${RUN_ID} \
-#     ${DATASET}"
-# echo "Running sbatch command ${SBATCH_COMMAND}"
-# sbatch ${SBATCH_COMMAND}
+# OPT
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_opt_experiment.slrm \
+    ${RUN_ID} \
+    ${DATASET}"
+echo "Running sbatch command ${SBATCH_COMMAND}"
+sbatch ${SBATCH_COMMAND}
 
-# # LLaMA
-# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment.slrm \
-#     ${RUN_ID} \
-#     ${DATASET}"
-# echo "Running sbatch command ${SBATCH_COMMAND}"
-# sbatch ${SBATCH_COMMAND}
+# LLaMA
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment.slrm \
+    ${RUN_ID} \
+    ${DATASET}"
+echo "Running sbatch command ${SBATCH_COMMAND}"
+sbatch ${SBATCH_COMMAND}
 
-# # Llama 2
-# SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama2_experiment.slrm \
-#     ${RUN_ID} \
-#     ${DATASET}"
-# echo "Running sbatch command ${SBATCH_COMMAND}"
-# sbatch ${SBATCH_COMMAND}
+# Llama 2
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama2_experiment.slrm \
+    ${RUN_ID} \
+    ${DATASET}"
+echo "Running sbatch command ${SBATCH_COMMAND}"
+sbatch ${SBATCH_COMMAND}
 
 # Llama 3
 SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama3_experiment.slrm \
