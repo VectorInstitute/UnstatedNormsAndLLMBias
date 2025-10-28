797366804a78271dcbb0852016c91cf09e6d606d
David Emerson <david.emerson@vectorinstitute.ai>
2025-10-28 13:47:05 -0400

    adding gemma experiments

--- unstated_norms_llm_bias/prompt_based_classification/launch_cot_prompt_experiments.sh
+++ unstated_norms_llm_bias/prompt_based_classification/launch_cot_prompt_experiments.sh
@@ -15,7 +15,7 @@
 RUN_ID=$1
 DATASET=$2
 
-# Run the ID and DATASET for each of the llama, llama2, llama3, Mistral, and Qwen models
+# Run the ID and DATASET for each of the llama, llama2, llama3, Mistral, Qwen, and Gemma models
 
 # LLaMA
 SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_llama_experiment_cot.slrm \
@@ -53,6 +53,13 @@ echo "Running sbatch command ${SBATCH_COMMAND}"
 sbatch ${SBATCH_COMMAND}
 
 SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_qwen2_5_7b_experiment_cot.slrm \
+    ${RUN_ID} \
+    ${DATASET}"
+echo "Running sbatch command ${SBATCH_COMMAND}"
+sbatch ${SBATCH_COMMAND}
+
+# Gemma
+SBATCH_COMMAND="unstated_norms_llm_bias/prompt_based_classification/slrm_scripts/run_gemma_7b_experiment_cot.slrm \
     ${RUN_ID} \
     ${DATASET}"
 echo "Running sbatch command ${SBATCH_COMMAND}"
